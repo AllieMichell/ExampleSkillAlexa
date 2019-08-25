b8e061de045163ea1ad8c44d77cf2f1270469635
Abner Estrada <joan.abner@hotmail.com>
2019-08-25 08:53:40 -0500

    trivia ciencias

---
+++ Trivias/ciencias.js
@@ -0,0 +1,84 @@
+module.exports = {
+    preguntas:[
+        // PREGUNTA 1
+        {
+            "question" : "¿Cómo se llama el componente mínimo que forma a los seres vivos?", 
+            "optiona":"Celula", //correcta
+            "optionb":"Particula",
+            "optionc":"Tejido",
+            "optiond":"Molecula"
+        },
+        {
+            // PREGUNTA 2
+            "question" : "¿Con qué respira una ballena?", 
+            "optiona":"pulmones", //correcta
+            "optionb":"branquias",
+            "optionc":"Piel", 
+            "optiond":"lengua"
+        },
+        {
+            // PREGUNTA 3
+            "question" : "Para los botánicos, el tomate es una:", 
+            "optiona":"hortaliza",
+            "optionb":"fruta", //correcta
+            "optionc":"Verdura",
+            "optiond":"leguminoza"
+        },
+        {
+            // PREGUNTA 4
+            "question" : "La fuerza física que la tierra ejerce sobre los cuerpos hacia su centro es la:", 
+            "optiona":"gravedad",//correcta
+            "optionb":"normal",
+            "optionc":"potencia",
+            "optiond":"velocidad" 
+        },
+        {
+            // PREGUNTA 5
+            "question" : "El sol es...", 
+            "optiona":"Un presidente",
+            "optionb":" un planeta",
+            "optionc":"una estrella", //correcto
+            "optiond":"una galaxia"
+        },
+        {
+            // PREGUNTA 6
+            "question" : "¿Qué tipo de radiación te produce quemaduras?", 
+            "optiona":"rayos x",
+            "optionb":"ultravioleta", //correcto
+            "optionc":"infrarojo",
+            "optiond":"solar"
+        },
+        {
+            // PREGUNTA 7
+            "question" : "¿¿Cuál fue el primer metal que el hombre empleó?", 
+            "optiona":"El acero",
+            "optionb":"El bibranio",
+            "optionc":"El oro",
+            "optiond":"El cobre" //correcto
+        },
+        {
+             // PREGUNTA 8
+            "question" : "¿Cómo se llama el proceso por el cual las plantas elaboran su alimento?", 
+            "optiona":"fotosíntesis", //correcta
+            "optionb":"polinización",
+            "optionc":"purga",
+            "optiond":"gestación"
+        },
+        {
+            // PREGUNTA 9
+            "question" : "¿Cuál es el satelite más importante de la tierra?", 
+            "optiona":"el satelite jooper",
+            "optionb":"marte",
+            "optionc":"la luna", //correcta
+            "optiond":"el sol"
+        },
+        {
+            // PREGUNTA 10
+            "question" : "¿Cuál es la ciencia que estudia las estrellas?", 
+            "optiona":"astronomía", //correcta
+            "optionb":"medicina",
+            "optionc":"astronautica",
+            "optiond":"la física"
+        }
+    ]
+}
